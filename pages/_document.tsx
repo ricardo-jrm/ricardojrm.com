01660e7bfcef3e12345067eb38e37a51b0377a41
Ricardo Mota <ricardoraposomota@gmail.com>
2019-12-05 02:21:29 +0000

    google tag management comment

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -82,8 +82,23 @@ class MyDocument extends Document {
           />
           {/* eslint-disable-next-line react/no-danger */}
           <script dangerouslySetInnerHTML={this.setGoogleAnalytics()} />
+
+          {/* <!-- Google Tag Manager -->
+          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-K45H59V');</script>
+          <!-- End Google Tag Manager --> */}
         </Head>
         <body>
+          {/* 
+            <!-- Google Tag Manager (noscript) -->
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K45H59V"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+            <!-- End Google Tag Manager (noscript) -->
+          */}
+
           <Main />
           <NextScript />
         </body>
